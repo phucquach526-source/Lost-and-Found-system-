2305e58d1e7cb1b1d2aba551841ecf87cba3dd30
phucquach526-source <phucquach526@gmail.com>
2025-11-27 19:35:54 -0500

    add foundCreate lostCreate html

--- project.js
+++ project.js
@@ -10,10 +10,18 @@ document.getElementById("found").addEventListener("click", () => {
 
 document.getElementById("begin").addEventListener("click", () => {
     if (selectType === "lost") {
-        window.location.href = "foundPage.html";
-    } else if (selectType === "found") {
         window.location.href = "lostPage.html";
+    } else if (selectType === "found") {
+        window.location.href = "foundPage.html";
     } else {
         alert("Please choose Found or Lost first");
     }
 });
+
+const createLostPostBtn = document.getElementById("createPost");
+if (createLostPostBtn) {
+    createLostPostBtn.addEventListener("click",  () => 
+        {window.location.href = "lostCreate.html" ;
+        });
+    
+}
